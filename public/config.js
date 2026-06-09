5a7c5595a21f76bdcb6126da7f05073d171e66e7
nanan910 <3245276254@qq.com>
2026-06-09 20:40:32 +0800

    Configure Cloudflare production backend

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
-window.MOVE_CAR_API_BASE = "";
-window.MOVE_CAR_DEMO_MODE = true;
+window.MOVE_CAR_API_BASE = "https://move-car-api.nanan910-move-car.workers.dev";
+window.MOVE_CAR_DEMO_MODE = false;
